414f0b4bf4ce893929a8e906f2c5c580f59d7a2a
Cristian Musat <32134094+musatcristian@users.noreply.github.com>
2023-02-18 15:40:28 +0200

    Testing GitHub actions

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import { Cipher } from "crypto";
+
 import { HELPER } from "./utils/index.js";
 
-const a = () => {
-  console.info(HELPER, process.env.SECRET);
-}
 
-a();
-Cipher;
+
+
+
+const a = (param) => {
+
+
+
+
+  console.info( HELPER, process.env.SECRET);
+};
+
+a(Cipher);
